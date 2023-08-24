5fdc91048cae2d091b22cf7afb681bbda5ced764
crybreak <gossanguy1@gmail.com>
2023-08-24 01:58:17 +0100

    map push

--- C_data_structure/Map/map.c
+++ C_data_structure/Map/map.c
@@ -9,10 +9,9 @@ unsigned int map_get_size(map_t map)
     if (map == NULL)
         return 0;
     for (count = 0; map->next != NULL; count++) {map = map->next;}
-    return (count + 1) ;
+    return (count + 1);
 }
-
-bool            map_is_empty(map_t map)
+bool           map_is_empty(map_t map)
 {
     return (map_get_size(map) == 0) ? false : true;
 }
@@ -24,11 +23,32 @@ static int key_comparator(void *first_key, void *second_key)
     else if (*((int *)first_key) > (*((int *)second_key)))
         return 1;
     else
-        return -1;
+        return (-1);
+}
+
+key_comparator_t key_cmp_container(bool store, key_comparator_t new_key_cmp)
+{
+    return new_key_cmp;
+}
+
+int pair_comparator(void *first, void *second)
+{
+    return  0;
+}
+
+bool map_add_elem(map_t *map_ptr, void *key, void *value, key_comparator_t key_cmp)
+{
+    return (true);
+}
+
+void map_get_elem(map_t map, void *key, key_comparator_t key_cmp)
+{
+
 }
 
 int main(void) {
     int a = 2;
     int b = 1;
     printf("%d", key_comparator(&a, &b));
+    printf
 }
--- C_data_structure/Map/map.h
+++ C_data_structure/Map/map.h
@@ -16,6 +16,7 @@ typedef struct pair
 {
     void    *key;
     void    *value;
+
 }   pair_t;
 
 typedef list_t map_t;
@@ -32,6 +33,7 @@ bool            map_is_empty(map_t map);
 key_comparator_t    key_cmp_container(bool   store, key_comparator_t new_key_cmp);
 int                 pair_comparator(void *first, void *second);
 
+
 /* Modification */
 
 bool    map_add_elem(map_t *map_ptr, void *key, void *value, key_comparator_t key_cmp);
@@ -39,6 +41,6 @@ bool    map_del_elem(map_t *map_ptr, void *key, key_comparator_t key_cmp);
 
 /* Access */
 
-void    *map_get_elem(map_t map, void *key, key_comparator_t key_cmp);
+void   map_get_elem(map_t map, void *key, key_comparator_t key_cmp);
 
 #endif /* !MAP_H_ */
